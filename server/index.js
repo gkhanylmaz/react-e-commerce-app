ef48a4f99cad3fc44f8ab8fcdc899780f5580acd
Gökhan <gokhanyilmaz997@gmail.com>
2023-03-04 21:34:44 +0300

    Product Server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+// Routes;
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -16,13 +19,11 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use("/api/products", productRoutes);
 
 //Create Port
-
 const PORT = process.env.PORT || 3001;
-
 //Listen
-
 app.listen(PORT, () => {
   console.log(`Server at running on the  port:http://localhost: ${PORT}`);
 });
