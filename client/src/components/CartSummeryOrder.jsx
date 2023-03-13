e1fd378bd6dcca0b2f3ea245b50a4d77060ba08e
Gökhan <gokhanyilmaz997@gmail.com>
2023-03-13 22:54:57 +0300

    Cart Summery Order

---
+++ client/src/components/CartSummeryOrder.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Slack,
+  Flex,
+  Box,
+  useColorModeValue as mode,
+  Text,
+  Heading,
+  Badge,
+} from "@chakra-ui/react";
+import { FaArrowRight } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { Link as ReactLink } from "react-router-dom";
+
+const CartSummeryOrder = () => {
+  return <div>CartSummeryOrder</div>;
+};
+
+export default CartSummeryOrder;
